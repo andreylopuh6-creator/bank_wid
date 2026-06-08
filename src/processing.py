fa64246af3d759252480ff98693195048a8982ac
Andrey Lopukhov <andreylopuh6@gmail.com>
2026-06-08 16:52:42 +0300

    update processing module with fixes

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,16 @@
-def filter_by_state(data, state+'EXECUTED'):
+def filter_by_state(data, state='EXECUTED'):
     '''
     Фильтр списка словарей по ключу.
     
     '''
+    result = []
+    for item in data:
+        if item.get('state') == state:
+            result.append(item)
+    return result
+
+def sort_by_date(data, reverse=True):
+    '''
+    Сортирует список операций по дате.
+    '''
+    return sorted(data, key=lambda x: x.get('date',''),reverse=reverse)
